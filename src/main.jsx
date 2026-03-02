c517a41760fd33e48ae68042390bc7dc0f1ecf08
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-02 07:35:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async"
+import App from './App'
+import './styles.css'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
+)
