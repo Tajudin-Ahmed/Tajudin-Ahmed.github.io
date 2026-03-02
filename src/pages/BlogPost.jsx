2668ba82eaf37d27a3c9d98d29e84c0a30bb0d7b
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-02 07:43:28 +0300

    Update BlogPost.jsx

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,1 +1,16 @@
+import { useParams } from "react-router-dom"
+import blogPosts from "../data/blogPosts"
 
+function BlogPost() {
+  const { slug } = useParams()
+  const post = blogPosts.find(p => p.slug === slug)
+
+  return (
+    <div className="container article">
+      <h1>{post.title}</h1>
+      <p style={{whiteSpace: "pre-line"}}>{post.content}</p>
+    </div>
+  )
+}
+
+export default BlogPost
