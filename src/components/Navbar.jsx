6c184566cb5364a19d42cff5cb927f9f9ec4d802
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-02 07:38:46 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+import { Link } from "react-router-dom"
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container nav-container">
+        <Link to="/" className="logo">Tajudin</Link>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/blog">Insights</Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
