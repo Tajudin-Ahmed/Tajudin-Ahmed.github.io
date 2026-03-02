19432a7f4ee522d69070bc9d1366d8c7e6a46c0d
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-02 07:43:04 +0300

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,1 +1,20 @@
+import { Link } from "react-router-dom"
+import blogPosts from "../data/blogPosts"
 
+function Blog() {
+  return (
+    <div className="container">
+      <h1>Analytics Insights</h1>
+      {blogPosts.map(post => (
+        <div key={post.slug} className="card">
+          <h3>{post.title}</h3>
+          <Link to={`/blog/${post.slug}`} className="btn">
+            Read Article
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Blog
