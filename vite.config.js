e7a1c19df9e5d5deaec60581b4c0dab5bbedd1c4
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-04 02:54:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   base: "/",
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000
+  }
 })
