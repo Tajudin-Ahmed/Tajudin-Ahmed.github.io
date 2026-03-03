a153b54818e80758d49c93a831395397c41b1e6e
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-04 00:56:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/Tajudin-Ahmed.github.io/",
+  base: "/",   // ✅ correct for user site
   plugins: [react()]
 })
