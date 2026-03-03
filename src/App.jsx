4b9b7a1d815d3a652280c152640287322698bd83
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-04 02:52:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import AOS from 'aos'
-import './App.css'
-
-// Initialize AOS
-AOS.init()
+import { Routes, Route, Navigate } from "react-router-dom"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
-    <HelmetProvider>
-      <Router>
-        <div className="app">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
-      </Router>
-    </HelmetProvider>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
 
-function Home() {
-  return (
-    <div>
-      <h1>Welcome to Tajudin's Portfolio</h1>
-      <p>React + Vite + React Router + AOS</p>
-    </div>
+      {/* Catch-all route */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   )
 }
 
