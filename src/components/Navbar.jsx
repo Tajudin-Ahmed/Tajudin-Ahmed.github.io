a6eee766883776c79152ae7d10f77e95348abb32
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-04 06:14:14 +0300

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/images/logo.png"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt="Tajudin Ahmed Logo" width="120" />
+    </nav>
+  )
+}
