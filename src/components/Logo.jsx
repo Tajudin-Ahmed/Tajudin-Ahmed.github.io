e77488b05f5bfd887de1fd5d90ddda1477740f6a
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-04 06:42:09 +0300

    Delete src/components/Logo.jsx

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/Logo.jsx
-export default function Logo() {
-  return (
-    <div className="logo">
-      <img src="/images/logo.png" alt="My Logo" height="40" />
-    </div>
-  );
-}
