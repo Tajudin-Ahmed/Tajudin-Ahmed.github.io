6b9fd544d2aa09d158037d4d0b1798422e62a14d
Tajudin A <92188273+Tajudin-Ahmed@users.noreply.github.com>
2026-03-04 05:27:01 +0300

    Create Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+// src/components/Logo.jsx
+export default function Logo() {
+  return (
+    <div className="logo">
+      <img src="/images/logo.png" alt="My Logo" height="40" />
+    </div>
+  );
+}
